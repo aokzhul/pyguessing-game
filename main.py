b1db9688933cb878d447a9b2b53bd8ffc7518b9a
Aokzhul <darkshak301br@gmail.com>
2025-03-27 21:58:16 -0300

    Adding attempts

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Description: Simple Python Number Guessing Game, for have fun
 from random import randint
 
+# How many attempts the player tried
+attempts = 0
 print('\nWELCOME TO PYGUESSING GAME\n\n')
 print('Hey there, What\'s up!?!\nWould you like to play a game? It\'s very simple, I think of a number between 0 and 100 and you try to guess what number I thought.')
 print('\nPS.: Type "exit" at any time, if you don\'t play anymore')
@@ -18,10 +20,11 @@ while True:
 		print('Oops, that doesn\'t look like a number. Please type again!')
 		continue
 
+	attempts += 1
 	if guess > secret_number:
 		print('The number I\'m thinking is lesser!')
 	elif guess < secret_number:
 		print('The number I\'m thinking is greater!')
 	else:
-		print('Wow, congratulations!! You guesset it!')
+		print(f'Wow, congratulations!! You guessed it in {attempts} attempts!')
 		break
