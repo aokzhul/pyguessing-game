dd123b350dd335557f22325580799334277a8e94
Aokzhul <darkshak301br@gmail.com>
2025-03-27 21:50:43 -0300

    Add an exit condition

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 from random import randint
 
 print('\nWELCOME TO PYGUESSING GAME\n\n')
-print('Hey there, What\'s up!?!\nWould like to play a game? It\'s very simple, I think a number between 0-100 and you try to guess what the number I think of.')
+print('Hey there, What\'s up!?!\nWould you like to play a game? It\'s very simple, I think of a number between 0 and 100 and you try to guess what number I thought.')
+print('\nPS.: Type "exit" at any time, if you don\'t play anymore')
 secret_number = randint(0, 100)
 
 while True:
+	guess = input('Type your guess: ')
+	if guess.lower() in ('quit', 'exit'):
+		print('Right, Thanks for playing! See you next time...')
+		break
 	try:
-		guess = int(input('Type your guess: '))
+		guess = int(guess)
 	except ValueError:
 		print('Oops, that doesn\'t look like a number. Please type again!')
 		continue
