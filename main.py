a04617f586de47b12d4fd26386e97cb88fcef474
Aokzhul <darkshak301br@gmail.com>
2025-03-27 22:00:03 -0300

    Encapsulating everything in a function

--- main.py
+++ main.py
@@ -3,28 +3,32 @@
 from random import randint
 
 # How many attempts the player tried
-attempts = 0
-print('\nWELCOME TO PYGUESSING GAME\n\n')
-print('Hey there, What\'s up!?!\nWould you like to play a game? It\'s very simple, I think of a number between 0 and 100 and you try to guess what number I thought.')
-print('\nPS.: Type "exit" at any time, if you don\'t play anymore')
-secret_number = randint(0, 100)
+def main():
+	attempts = 0
+	print('\nWELCOME TO PYGUESSING GAME\n\n')
+	print('Hey there, What\'s up!?!\nWould you like to play a game? It\'s very simple, I think of a number between 0 and 100 and you try to guess what number I thought.')
+	print('\nPS.: Type "exit" at any time, if you don\'t play anymore')
+	secret_number = randint(0, 100)
 
-while True:
-	guess = input('Type your guess: ')
-	if guess.lower() in ('quit', 'exit'):
-		print('Right, Thanks for playing! See you next time...')
-		break
-	try:
-		guess = int(guess)
-	except ValueError:
-		print('Oops, that doesn\'t look like a number. Please type again!')
-		continue
+	while True:
+		guess = input('Type your guess: ')
+		if guess.lower() in ('quit', 'exit'):
+			print('Right, Thanks for playing! See you next time...')
+			break
+		try:
+			guess = int(guess)
+		except ValueError:
+			print('Oops, that doesn\'t look like a number. Please type again!')
+			continue
 
-	attempts += 1
-	if guess > secret_number:
-		print('The number I\'m thinking is lesser!')
-	elif guess < secret_number:
-		print('The number I\'m thinking is greater!')
-	else:
-		print(f'Wow, congratulations!! You guessed it in {attempts} attempts!')
-		break
+		attempts += 1
+		if guess > secret_number:
+			print('The number I\'m thinking is lesser!')
+		elif guess < secret_number:
+			print('The number I\'m thinking is greater!')
+		else:
+			print(f'Wow, congratulations!! You guessed it in {attempts} attempts!')
+			break
+
+if __name__ == '__main__':
+	main()
